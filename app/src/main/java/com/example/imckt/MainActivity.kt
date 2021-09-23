100b6fa0420d0b80711026a25e3a15c98f299415
João Lucas Pereira Rafael <61843626+joao-lusa@users.noreply.github.com>
2021-09-23 15:31:19 -0300

    rodando

--- app/src/main/java/com/example/imckt/MainActivity.kt
+++ app/src/main/java/com/example/imckt/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.imckt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.imckt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+        bind.btnCalcular.setOnClickListener{
+            val valorPeso = bind.numPeso.text.toString().toDouble()
+            val valorAltura = bind.numAltura.text.toString().toDouble()
+            val calculoImc = CalculoImc(valorPeso, valorAltura)
+            Toast.makeText(this,calculoImc.calcular(), Toast.LENGTH_LONG).show()//52:32 da aula
+
+        }
+
 
     }
 }
